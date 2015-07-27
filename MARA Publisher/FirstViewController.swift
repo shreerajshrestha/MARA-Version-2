33bdcd013e3970f470ab4cc1fd65aa9e21920496
Shree Raj Shrestha <sshrestha@rollins.edu>
2015-07-27 02:14:38 -0400

    Initial Commit

---
+++ MARA Publisher/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MARA Publisher
+//
+//  Created by Shree Raj Shrestha on 7/27/15.
+//  Copyright (c) 2015 Shree Raj Shrestha. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MARA PublisherTests/MARA_PublisherTests.swift
@@ -0,0 +1,36 @@
+//
+//  MARA_PublisherTests.swift
+//  MARA PublisherTests
+//
+//  Created by Shree Raj Shrestha on 7/27/15.
+//  Copyright (c) 2015 Shree Raj Shrestha. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MARA_PublisherTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
