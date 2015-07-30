d99738e4e3047be8f21352a0afedb81feea241e9
Shree Raj Shrestha <sshrestha@rollins.edu>
2015-07-29 20:30:57 -0400

    added content view

--- MARA Publisher/AddMediaViewController.swift
+++ MARA Publisher/AddMediaViewController.swift
@@ -6,20 +6,38 @@
 //  Copyright (c) 2015 Shree Raj Shrestha. All rights reserved.
 //
 
+import Foundation
 import UIKit
 import MobileCoreServices
+import CoreLocation
+import MediaPlayer
 
-class AddMediaViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class AddMediaViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLLocationManagerDelegate {
     
+    @IBOutlet weak var preview: UIView!
+    @IBOutlet weak var imagePreview: UIImageView!
+    @IBOutlet weak var saveAsTextField: UITextField!
+    @IBOutlet weak var tagsTextField: UITextField!
+    @IBOutlet weak var latitudeTextField: UITextField!
+    @IBOutlet weak var longitudeTextField: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var contentViewHeight: NSLayoutConstraint!
     
     var titleText = String()
     var mediaType = String()
-    var mediaURL = NSURL()
+    var fileManager = NSFileManager()
+    var camera = UIImagePickerController()
+    var videoPlayer: MPMoviePlayerController!
+    var tempPath = String()
+    var locationManager = CLLocationManager()
+    var latitude = Double()
+    var longitude = Double()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = titleText
+        camera.delegate = self
+        locationManager.delegate = self
     }
     
     override func viewDidLayoutSubviews() {
@@ -29,11 +47,9 @@ class AddMediaViewController: UIViewController, UIImagePickerControllerDelegate,
     
     func showCamera() {
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
-            var camera = UIImagePickerController()
             camera.sourceType = UIImagePickerControllerSourceType.Camera
             camera.allowsEditing = false
-            camera.delegate = self
-            
+            camera.showsCameraControls = true
             if mediaType == "image" {
                 camera.mediaTypes = [kUTTypeImage]
             } else if mediaType == "video" {
@@ -43,8 +59,31 @@ class AddMediaViewController: UIViewController, UIImagePickerControllerDelegate,
         }
     }
     
-    func saveCameraCapture() {
-        
+    func loadPreview() {
+        switch mediaType {
+            
+        case "image":
+            if let tempImage = UIImage(contentsOfFile: tempPath) {
+                imagePreview.image = tempImage
+            }
+        case "video":
+            var videoURL: NSURL! = NSURL(fileURLWithPath: tempPath)
+            videoPlayer = MPMoviePlayerController(contentURL: videoURL)
+            if let player = videoPlayer {
+                var screenSize: CGSize = UIScreen.mainScreen().bounds.size as CGSize
+                player.view.frame = CGRect(x: 0, y: 0, width: screenSize.width - 50, height: preview.frame.height)
+                player.view.center.x = preview.center.x
+                player.view.center.y = preview.center.y
+                player.scalingMode = MPMovieScalingMode.AspectFit
+                player.controlStyle = MPMovieControlStyle.Embedded
+                player.movieSourceType = MPMovieSourceType.File
+                player.play()
+                player.pause()
+                preview.addSubview(player.view)
+            }
+        default:
+            println("Handle other cases")
+        }
     }
     
     @IBAction func cancelButtonPressed(sender: UIBarButtonItem) {
@@ -60,6 +99,37 @@ class AddMediaViewController: UIViewController, UIImagePickerControllerDelegate,
         }
     }
     
+    @IBAction func getLocationButtonTapped(sender: UIButton) {
+        switch CLLocationManager.authorizationStatus() {
+            
+        case .NotDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            
+        case .Restricted, .Denied:
+            let alert = UIAlertController(title: "Location Access Disabled",
+                message: "Location data is required to save media. Please open settings for this app and set location access to \n'While Using the App'.",
+                preferredStyle: UIAlertControllerStyle.Alert)
+            let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+            let openSettingsAction = UIAlertAction(title: "Open Settings", style: .Default) { (action) in
+                if let url = NSURL(string:UIApplicationOpenSettingsURLString) {
+                    UIApplication.sharedApplication().openURL(url)
+                }
+            }
+            alert.addAction(cancelAction)
+            alert.addAction(openSettingsAction)
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+        default:
+            locationManager.distanceFilter = kCLDistanceFilterNone
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    @IBAction func saveButtonTapped(sender: UIBarButtonItem) {
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -71,14 +141,57 @@ class AddMediaViewController: UIViewController, UIImagePickerControllerDelegate,
     */
     
     // MARK: - Image Picker Controller
+    
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
-        mediaURL = info[UIImagePickerControllerMediaURL] as! NSURL
-        println(mediaURL)
+        switch mediaType {
+        case "image":
+            let capturedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+            tempPath = NSTemporaryDirectory().stringByAppendingString("temp.jpg")
+            fileManager.removeItemAtPath(tempPath, error: nil)
+            UIImageJPEGRepresentation(capturedImage, 1.0).writeToFile(tempPath, atomically: true)
+        case "video":
+            var capturedVideoURL: NSURL = info[UIImagePickerControllerMediaURL] as! NSURL
+            tempPath = NSTemporaryDirectory().stringByAppendingString("temp.mp4")
+            var tempURL: NSURL! = NSURL(fileURLWithPath: tempPath)
+            fileManager.removeItemAtURL(tempURL, error: nil)
+            fileManager.copyItemAtURL(capturedVideoURL, toURL: tempURL, error: nil)
+        default:
+            dismissViewControllerAnimated(true, completion: nil)
+        }
+        loadPreview()
         dismissViewControllerAnimated(true, completion: nil)
     }
     
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK:- Location Manager
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        if let location: CLLocation = locations.last as? CLLocation {
+            latitude = location.coordinate.latitude
+            longitude = location.coordinate.longitude
+            latitudeTextField.text = "\(latitude)"
+            longitudeTextField.text = "\(longitude)"
+            locationManager.stopUpdatingLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+        let alert = UIAlertController(
+            title: "Error Accessing Location",
+            message: "An unexpected error occured while accessing location data. Please open settings and verify that the location access for this app is set to \n'While Using the App'.",
+            preferredStyle: UIAlertControllerStyle.Alert)
+        let cancelAction = UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default, handler: nil)
+        let openSettingsAction = UIAlertAction(title: "Open Settings", style: .Default) { (action) in
+            if let url = NSURL(string:UIApplicationOpenSettingsURLString) {
+                UIApplication.sharedApplication().openURL(url)
+            }
+        }
+        alert.addAction(cancelAction)
+        alert.addAction(openSettingsAction)
+        self.presentViewController(alert, animated: true, completion: nil)
     }
 
 }
